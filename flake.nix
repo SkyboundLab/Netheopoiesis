5edddf46fb7c9859509415c897cc6c5adf272217
Prince527 <Prince527gaming@gmail.com>
2025-01-15 15:31:18 -0400

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Netheopoiesis";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = { self, systems, nixpkgs }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = eachSystem (system: 
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              jdk
+              maven
+            ];
+          };
+        }
+      );
+    };
+}
